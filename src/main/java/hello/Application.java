c421556bcf3f4fb3206c157f013caa4a687becab
Manuel Pettersson <Manuel.Pettersson@postkodlotteriet.se>
2016-11-14 08:55:47 +0100

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+//källa: https://spring.io/guides/gs/accessing-twitter/
+
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
